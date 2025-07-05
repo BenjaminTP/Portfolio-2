6f54ebcc46c71e392b336b8a3d4fb7782028c34f
BenjaminTP <benjamintranpugh@gmail.com>
2025-07-05 01:12:23 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     let card = document.querySelector("#project1")
+//     card.addEventListener('click', function() {
+//         card.style.backgroundColor = 'red';
+//     });
+// });
